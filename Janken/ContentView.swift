52d43f9affdd44074c8f07913fd8cb4454820746
mas <m.yoshida553@gmail.com>
2025-01-24 20:10:30 +0900

    Initial Commit

---
+++ Janken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Janken
+//
+//  Created by masato yoshida on 2025/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by masato yoshida on 2025/01/24.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
